e5b6b07ba87b4482ad53a66da91c4bf0b2cc0962
JamieAC1108 <jaaroncole2004@gmail.com>
2025-02-04 15:26:51 -0500

    Commit

--- assignment_3.py
+++ assignment_3.py
@@ -7,9 +7,32 @@
 
 
 
+numbers_list = [1, 2, 4, 9, 10]
+
+print('Numbers to sum: ', numbers_list)
+
+result = sum(numbers_list)
+
+print(f"The sum is: {result}")
+
+
 # Problem 2: Write a program that takes a list of numbers and prints out the average of all the numbers in the list.
 
+numbers_list = [1, 2, 3, 4, 10]
 
+print('Numbers to average: ', numbers_list)
+
+result = sum(numbers_list)/len(numbers_list)
+
+print(f'The average is: {result}')
 
 # Problem 3: Write a program that takes a list of numbers and prints out the largest number in the list.
+
+numbers_list = [200, 700, 400, 500, 250]
+
+print('Finding the highest number of:', numbers_list)
+
+result = max(numbers_list)
+
+print(f'The highest number in the set is: {result}')
 
